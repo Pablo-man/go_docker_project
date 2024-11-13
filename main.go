5262d951afa97d6c84c164395efa3eab6d80d520
Pablo Méndez <amandrangopablo@gmail.com>
2024-11-13 12:24:32 -0500

    feat: :art: New message for the web site
    change hola mundo for hello world with go

--- main.go
+++ main.go
@@ -1,16 +1,16 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
+	"fmt"
+	"net/http"
 )
 
 func helloHandler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "¡Hola, mundo desde Go!")
+	fmt.Fprintf(w, "¡Hello world with go!")
 }
 
 func main() {
-    http.HandleFunc("/", helloHandler)
-    fmt.Println("Servidor ejecutándose en http://localhost:8080")
-    http.ListenAndServe(":8080", nil)
+	http.HandleFunc("/", helloHandler)
+	fmt.Println("Servidor ejecutándose en http://localhost:8080")
+	http.ListenAndServe(":8080", nil)
 }
